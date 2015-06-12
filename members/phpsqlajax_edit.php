9be515042c74538a4e63ee047efa96d1e5276e68
Panayiotis Pavlides <ppavli02@cs.ucy.ac.cy>
2015-06-12 12:21:05 +0300

    sql injections

--- members/phpsqlajax_edit.php
+++ members/phpsqlajax_edit.php
@@ -23,61 +23,16 @@
         if($connection->affected_rows==1){
 
             $json_return = json_encode($row);
-            
+
         }
 
 
         echo $json_return;
         $stmt->close();
     }
-//
-//    $query = "SELECT * from $database.MEMBER WHERE email='$email'";
-//    $result = mysql_query($query);
-//
-//    $json_return;
-//
-//    while ($row = @mysql_fetch_assoc($result)){
-//        $json_return = json_encode($row);
-//    }
-//
-//    echo $json_return;
+
         
 ?>
 
 
 
-
-
-
-if($stmt = $connection->prepare("SELECT * from $database.MEMBER WHERE email=? AND password=?")){
-$stmt->bind_param("ss", $email,$user_password);
-$stmt->execute();
-$result = $stmt->get_result();
-$row= $result->fetch_array(MYSQLI_ASSOC);
-
-
-$mple=0;
-$prasino=0;
-$poios=-1;
-
-$mple=$row['pre74'];
-$prasino=$row['post74'];
-$poios=$row['id'];
-
-if($connection->affected_rows==1){
-$_SESSION["email"]=$email;
-$_SESSION["password"]=$user_password;
-$_SESSION["logged_in"]=1;
-$_SESSION["errorLogin"]=null;
-$_SESSION["pre74"]=$mple;
-$_SESSION["post74"]=$prasino;
-$_SESSION["member"]=$poios;
-header("location:login_success.php");
-}else{
-$_SESSION["errorLogin"]=1;
-header("location:../../index.php");
-}
-ob_end_flush();
-
-$stmt->close();
-}
