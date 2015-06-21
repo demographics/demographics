1007c6b6fff7ca8b4f5637a488b8742a79f14d96
Demetris Paschalides <dpasch01@cs.ucy.ac.cy>
2015-06-21 18:24:15 +0300

    rename.sql added,.

--- database/initialize.sql
+++ database/initialize.sql
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `comment` (
   `content` text NOT NULL,
   `datetime` datetime NOT NULL,
   `member` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+);
 
 --
 -- Dumping data for table `comment`
@@ -58,7 +58,7 @@ INSERT INTO `comment` (`id`, `event`, `content`, `datetime`, `member`) VALUES
 CREATE TABLE IF NOT EXISTS `community` (
   `id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+);
 
 --
 -- Dumping data for table `community`
@@ -75,17 +75,18 @@ INSERT INTO `community` (`id`, `name`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `event` (
-  `id` int(11) NOT NULL,
-  `title` varchar(20) NOT NULL,
-  `type` enum('memoir','article','property','photo') NOT NULL,
-  `content` varchar(800) NOT NULL,
-  `datetime` datetime NOT NULL,
-  `date` date NOT NULL,
-  `view` int(11) NOT NULL,
-  `like` int(11) NOT NULL,
-  `member` int(11) NOT NULL,
-  `tags` text
-) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=latin1;
+    `id` int(11) NOT NULL,
+    `title` varchar(20) NOT NULL,
+    `type` enum('memoir','article','property','photo') NOT NULL,
+    `content` varchar(800) NOT NULL,
+    `datetime` datetime NOT NULL,
+    `date` date NOT NULL,
+    `view` int(11) NOT NULL,
+    `like` int(11) NOT NULL,
+    `member` int(11) NOT NULL,
+    tags TEXT,
+    FULLTEXT(title, tags)
+);
 
 --
 -- Dumping data for table `event`
@@ -109,7 +110,7 @@ CREATE TABLE IF NOT EXISTS `marker` (
   `event` int(11) NOT NULL,
   `lng` float(10,6) NOT NULL,
   `lat` float(10,6) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
 --
 -- Dumping data for table `marker`
@@ -140,7 +141,7 @@ CREATE TABLE IF NOT EXISTS `member` (
   `pre74` int(11) NOT NULL,
   `post74` int(11) NOT NULL,
   `picture` varchar(60) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+);
 
 --
 -- Dumping data for table `member`
@@ -172,7 +173,7 @@ CREATE TABLE IF NOT EXISTS `notification` (
   `status` enum('seen','unseen') NOT NULL,
   `type` int(11) NOT NULL,
   `datetime` datetime NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=1155 DEFAULT CHARSET=utf8;
+);
 
 --
 -- Dumping data for table `notification`
@@ -220,7 +221,7 @@ CREATE TABLE IF NOT EXISTS `reply` (
   `content` text NOT NULL,
   `datetime` datetime NOT NULL,
   `member` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+);
 
 --
 -- Dumping data for table `reply`
@@ -243,7 +244,7 @@ CREATE TABLE IF NOT EXISTS `road` (
   `name` varchar(20) NOT NULL,
   `path` text NOT NULL,
   `member` int(11) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=1174 DEFAULT CHARSET=utf8;
+);
 
 --
 -- Dumping data for table `road`
@@ -271,7 +272,7 @@ CREATE TABLE IF NOT EXISTS `road_comment` (
   `content` text NOT NULL,
   `datetime` datetime NOT NULL,
   `member` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+);
 
 --
 -- Dumping data for table `road_comment`
@@ -303,7 +304,7 @@ CREATE TABLE IF NOT EXISTS `subject` (
   `view` int(11) NOT NULL,
   `community` int(11) NOT NULL,
   `member` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=latin1;
+);
 
 --
 -- Dumping data for table `subject`
@@ -361,7 +362,7 @@ ALTER TABLE `community`
 -- Indexes for table `event`
 --
 ALTER TABLE `event`
-  ADD PRIMARY KEY (`id`), ADD KEY `member` (`member`), ADD FULLTEXT KEY `title` (`title`,`tags`);
+  ADD PRIMARY KEY (`id`), ADD KEY `member` (`member`);
 
 --
 -- Indexes for table `marker`
